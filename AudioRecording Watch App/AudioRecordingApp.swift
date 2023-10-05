0fa84d6b4777879c04a5cea65c3a07603f5e9846
Kenny Jinhiro <jxnhiro@gmail.com>
2023-10-05 13:52:41 +0700

    Initial Commit

---
+++ AudioRecording Watch App/AudioRecordingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioRecordingApp.swift
+//  AudioRecording Watch App
+//
+//  Created by Kenny Jinhiro Wibowo on 05/10/23.
+//
+
+import SwiftUI
+
+@main
+struct AudioRecording_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AudioRecording Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AudioRecording Watch App
+//
+//  Created by Kenny Jinhiro Wibowo on 05/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
